2a4cb108e77557630b6aee80f35a75429cf5228c
guifavere <guilherme.favere@gmail.com>
2021-03-24 19:51:18 -0300

    Config sort commits

--- src/HTMLCommitsManager.ts
+++ src/HTMLCommitsManager.ts
@@ -7,35 +7,41 @@ interface CommitLink extends HTMLLinkElement {
   ariaLabel: string;
 }
 
+type Sort = 'ASC' | 'DESC';
+
 export default class HTMLCommitsManager implements CommitsManager {
-  public block: HTMLElement;
+  private block: HTMLElement;
+
+  private commitsList: HTMLOListElement;
+
+  private commitLinks: NodeListOf<CommitLink>;
+
+  private sort: Sort = 'DESC';
 
   constructor(block: HTMLElement) {
     this.block = block;
+    this.commitsList = this.getCommitsList(block);
+    this.commitLinks = this.getCommitLinks(block);
 
     this.init();
   }
 
-  public async copyCommits(): Promise<void> {
-    const commitLinks = this.getCommitLinks();
-
-    let clipboardContent = '';
-
-    commitLinks.forEach((commitLink) => {
-      const message = `${commitLink.textContent}\n`;
-
-      clipboardContent += message;
-    });
-
-    await navigator.clipboard.writeText(clipboardContent);
+  private getCommitsList(block: HTMLElement): HTMLOListElement {
+    return block.querySelector('.TimelineItem-body > ol') as HTMLOListElement;
   }
 
-  public sortCommits(): void {
-    console.log('sortCommits');
+  private getCommitLinks(block: HTMLElement): NodeListOf<CommitLink> {
+    return block.querySelectorAll('li > div > p > a');
   }
 
-  private getCommitLinks(): NodeListOf<CommitLink> {
-    return this.block.querySelectorAll('li > div > p > a');
+  private handleSortCommitsASC(): void {
+    this.sort = 'ASC';
+    this.commitsList.style.flexDirection = 'column-reverse';
+  }
+
+  private handleSortCommitsDESC(): void {
+    this.sort = 'DESC';
+    this.commitsList.style.flexDirection = 'column';
   }
 
   private makeCopyButton(): HTMLButtonElement {
@@ -47,7 +53,6 @@ export default class HTMLCommitsManager implements CommitsManager {
       display: flex;
       height: 32px;
       justify-content: center;
-      margin: 0 0 0 auto;
       width: 32px;
     `;
 
@@ -63,6 +68,51 @@ export default class HTMLCommitsManager implements CommitsManager {
     return button;
   }
 
+  private makeSortButton(): HTMLButtonElement {
+    const button = document.createElement('button');
+    const buttonStyle = `
+      align-items: center;
+      background: #161b22;
+      border: 0;
+      display: flex;
+      height: 32px;
+      justify-content: center;
+      width: 32px;
+    `;
+
+    const iconSource = chrome.extension.getURL('assets/MdContentCopy.svg');
+
+    button.setAttribute('type', 'button');
+    button.setAttribute('title', 'sort commits');
+    button.onclick = this.sortCommits.bind(this);
+
+    button.style.cssText = buttonStyle;
+    button.innerHTML = `<img src=${iconSource} />`;
+
+    return button;
+  }
+
+  private makeToolbar(): HTMLElement {
+    const toolbarStyle = `
+      border-bottom: 1px solid #161b22;
+      display: flex;
+      gap: 15px;
+      justify-content: flex-end;
+      margin-bottom: 15px;
+      padding-bottom: 15px;
+    `;
+
+    const copyButton = this.makeCopyButton();
+    const sortButton = this.makeSortButton();
+
+    const toolbar = document.createElement('section');
+
+    toolbar.style.cssText = toolbarStyle;
+    toolbar.append(sortButton, copyButton);
+
+    return toolbar;
+  }
+
   private setFullMessage(commitLink: CommitLink): void {
     const fullMessage = commitLink.ariaLabel;
 
@@ -70,10 +120,29 @@ export default class HTMLCommitsManager implements CommitsManager {
   }
 
   private init(): void {
-    const commitLinks = this.getCommitLinks();
-    const copyButton = this.makeCopyButton();
+    this.block.insertAdjacentElement('beforebegin', this.makeToolbar());
 
-    this.block.insertAdjacentElement('beforebegin', copyButton);
-    commitLinks.forEach((commitLink) => this.setFullMessage(commitLink));
+    this.commitsList.style.display = 'flex';
+    this.commitsList.style.flexDirection = 'column';
+
+    this.commitLinks.forEach((commitLink) => this.setFullMessage(commitLink));
+  }
+
+  public async copyCommits(): Promise<void> {
+    let clipboardContent = '';
+
+    this.commitLinks.forEach((commitLink) => {
+      const message = `${commitLink.textContent}\n`;
+
+      clipboardContent += message;
+    });
+
+    await navigator.clipboard.writeText(clipboardContent);
+  }
+
+  public sortCommits(): void {
+    this.sort === 'ASC'
+      ? this.handleSortCommitsDESC()
+      : this.handleSortCommitsASC();
   }
 }
